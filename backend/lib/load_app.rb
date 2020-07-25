3be4394e4593879316de6187509f9b159f543f8e
M.Bellucci <delbetu@gmail.com>
2020-07-25 11:15:52 -0300

    move token

--- backend/lib/load_app.rb
+++ backend/lib/load_app.rb
@@ -5,6 +5,6 @@ $LOAD_PATH << "#{APP_ROOT}/app"
 module App
 end
 require 'authorizer'
-require 'token'
+require 'shared/domain/token'
 require 'sign_in/domain/user_credentials'
 require 'sign_in/flow'
