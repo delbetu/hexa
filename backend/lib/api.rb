564cb4fe27078251fe77feee621fb5fde5966e82
M.Bellucci <delbetu@gmail.com>
2020-08-01 09:10:25 -0300

    remove not needed rest for users

--- backend/lib/api.rb
+++ backend/lib/api.rb
@@ -1,9 +1,36 @@
 require 'sinatra'
-require_relative 'api/v1/users'
 require_relative 'sign_in/infrastructure/api_v1'
 require_relative 'sign_up/infrastructure/api_v1'
 
 before do
   headers 'Access-Control-Allow-Origin' => 'http://localhost:8080'
   headers 'Access-Control-Allow-Origin' => '*'
+end
+
+error do
+  # Server error 5xx
+  {
+    status: 500,
+    errors: [
+      {
+        code: '5xx',
+        title: "Not Found",
+        detail: "Entity not found"
+      }
+    ],
+  }.to_json
+end
+
+not_found do
+  # Client error 4xx
+  {
+    status: 404,
+    errors: [
+      {
+        code: '4xx',
+        title: "Not Found",
+        detail: "Entity not found"
+      }
+    ],
+  }.to_json
 end
