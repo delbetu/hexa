5f73eacba24ae542995eb672ae2520da112dfdf5
M.Bellucci <delbetu@gmail.com>
2020-08-15 08:38:19 -0300

    make hexa only backend app

--- lib/sign_up/infrastructure/api_v1.rb
+++ lib/sign_up/infrastructure/api_v1.rb
@@ -1,5 +1,6 @@
 require 'sign_up/app/sign_up'
 require 'sign_up/infrastructure/user_creator_adapter'
+require 'logger'
 
 class UsersSerializer
   include FastJsonapi::ObjectSerializer
@@ -9,6 +10,9 @@ end
 post '/users' do
   body = request.body.read.to_s
   user_attributes = params[:user] || JSON.parse(body)['user'] # accept form data or json
+
+  Logger.new("log/development.log").error("[API user attributes]")
+  Logger.new("log/development.log").error(user_attributes)
   new_user = SignUp.call(user_attributes, creator: UserCreatorAdapter)
   UsersSerializer.new(new_user).serialized_json
 end
