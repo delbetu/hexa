a43789750d88fcacedeb1ea95c6d923d60c0360a
M.Bellucci <delbetu@gmail.com>
2020-10-11 18:12:17 -0300

    Add resource generator

--- lib/extensions.rb
+++ lib/extensions.rb
@@ -1,9 +1,20 @@
+# frozen_string_literal: true
+
 require 'shared/errors'
 
 class String
   def singularize
-    return chop if downcase.end_with? "s"
+    return chop if downcase.end_with? 's'
+
     self
+  end
+
+  def underscore
+    gsub(/::/, '/')
+      .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+      .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+      .tr('-', '_')
+      .downcase
   end
 end
 
@@ -26,7 +37,7 @@ class NullObject
     @origin
   end
 
-  def method_missing(*args, &block)
+  def method_missing(*_args)
     self
   end
 
@@ -39,24 +50,24 @@ def Maybe(value)
   value.nil? ? NullObject.new : value
 end
 
-def assert(condition, message = "Assertion failed")
+def assert(condition, message = 'Assertion failed')
   raise EndUserError, message, caller unless condition
 end
 
-def with_error_handling(error_id = "Error", &block)
-  block.call
+def with_error_handling(error_id = 'Error')
+  yield
 rescue EndUserError => e
-  ActionError(id: error_id, errors: [ e.message ])
-rescue => e
-  if (env == 'production')
+  ActionError(id: error_id, errors: [e.message])
+rescue StandardError => e
+  if env == 'production'
     Raven.capture_exception(e)
 
     # End user doesn't receive a stacktrace
     ActionError(id: error_id, errors: [
-      "An error has occured. "\
-      "It's been reported and we will be working on it soon. "\
-      "Please try again later."
-    ])
+                  'An error has occured. '\
+                  "It's been reported and we will be working on it soon. "\
+                  'Please try again later.'
+                ])
   else # test & Development
     raise e # Developer needs to track the issue.
   end
